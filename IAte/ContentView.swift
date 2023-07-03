1a50856819c0dd8028d4889ccd9d5f596fb4e6b9
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-07-03 09:49:21 +0200

    Initial Commit

---
+++ IAte/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IAte
+//
+//  Created by DA M1 MAC 89 on 2023/07/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IAte/IAteApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAteApp.swift
+//  IAte
+//
+//  Created by DA M1 MAC 89 on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct IAteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
