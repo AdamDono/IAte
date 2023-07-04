450d2b8f84fcf3b3bd68f406332f260d5b3b3c4a
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-07-04 10:28:46 +0200

    added textfield

---
+++ IAte/ Views/AddFoodview.swift
@@ -0,0 +1,20 @@
+//
+//  AddFoodview.swift
+//  IAte
+//
+//  Created by DA M1 MAC 89 on 2023/07/04.
+//
+
+import SwiftUI
+
+struct AddFoodview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddFoodview_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodview()
+    }
+}
